d85bd7b4abdd146ca8e90cb8e698c81045e9b9cb
Nikola <nikolaviki@hotmail.com>
2020-06-18 18:40:13 +0200

    creating header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-import { Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 const HatsPage = () => (
   <div>
@@ -12,8 +13,11 @@ const HatsPage = () => (
 function App() {
   return (
     <div className="App">
-      <Route exact path="/" component={HomePage} />
-      <Route exact path="/shop" component={ShopPage} />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
