5554c63696e3450250eb7ff2dad8e4dfea659269
Nikola <nikolaviki@hotmail.com>
2020-06-25 18:16:16 +0200

    changes on shop size photo..

--- src/components/contact/contact.component.jsx
+++ src/components/contact/contact.component.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => (
     <div>
-        <h2>OVO VIKTORE STO VIDIS SAM NAPRAVIO LICNO JA!</h2>
+        <h2>WORK IN PROGRES!</h2>
     </div>
 )
 export default Contact
